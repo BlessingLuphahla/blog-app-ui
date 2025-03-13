09160df27518be1e71f1296108bc3d18e9780c35
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-13 18:02:20 +0200

    axios update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
+
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/api"
+    : "https://blog-app-api-7q63.onrender.com/api";
 
 const http = axios.create({
-  baseURL: 'http://localhost:5000/api'
-})
+  baseURL: BASE_URL,
+});
 
-export default http
+export default http;
