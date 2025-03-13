cc7e00a8f145e608e6ee7286235bf8c164e80466
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-13 19:17:01 +0200

    misc update

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,53 +1,52 @@
-import { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { Context } from '../../context/Context'
-import './topbar.css'
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./topbar.css";
 
 export default function TopBar() {
-    const publicFolder = 'http://localhost:5000/images/profiles/';
-    const { user, dispatch } = useContext(Context);
+  const publicFolder = "http://localhost:5000/images/profiles/";
+  const { user, dispatch } = useContext(Context);
 
-    const handleLogout = () => {
-        dispatch({ type: 'LOGOUT' });
-    }
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
 
-    return (
-        <div className='top'>
-            <div className="topCenter">
-                <ul className="topList">
-                    <li className='topListItem'>
-                        <Link to={'/'}>HOME</Link>
-                    </li>
-                    <li className='topListItem'>
-                        <Link to={'/about'}>ABOUT</Link>
-                    </li>
-                    <li className='topListItem'>
-                        <Link to={'/contact'}>CONTACT</Link>
-                    </li>
-                    {
-                        user &&
-                        <li className='topListItem'>
-                            <Link to={'/create'}>WRITE</Link>
-                        </li>
-                    }
-                    {
-                        user &&
-                        <li className='topListItem' onClick={handleLogout}>LOGOUT</li>
-                    }
+  return (
+    <div className="top">
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link to={"/"}>HOME</Link>
+          </li>
+          <li className="topListItem">
+            <Link to={"/about"}>ABOUT</Link>
+          </li>
+          <li className="topListItem">
+            <Link to={"/contact"}>CONTACT</Link>
+          </li>
+          {user && (
+            <li className="topListItem">
+              <Link to={"/create"}>WRITE</Link>
+            </li>
+          )}
+          {user && (
+            <li className="topListItem" onClick={handleLogout}>
+              LOGOUT
+            </li>
+          )}
 
-                    {/* <li className='topListItem'>
+          {/* <li className='topListItem'>
                     {user && 'LOGOUT'}
                     <Link to={'/logout'}>LOGOUT</Link>
                 </li> */}
 
-                    {/* <li className='topListItem'>HOME</li>
+          {/* <li className='topListItem'>HOME</li>
                 <li className='topListItem'>ABOUT</li>
                 <li className='topListItem'>CONTACT</li>
                 <li className='topListItem'>WRITE</li>
                 <li className='topListItem'>LOGOUT</li> */}
 
-
-                    {/* <Link to={'/'}>
+          {/* <Link to={'/'}>
                     <li className='topListItem'>HOME</li>
                 </Link>
                 <Link to={'/about'}>
@@ -62,37 +61,46 @@ export default function TopBar() {
                 <Link to={'/logout'}>
                     <li className='topListItem'>LOGOUT</li>
                 </Link> */}
-                </ul>
-            </div>
-            <div className="topRight">
-                {user ? (
-                    <Link to={'/settings'}>
-                        {user.profileImage ?
-                            <img
-                                className='topImage'
-                                src={publicFolder + user.profileImage}
-                                alt="avatar"
-                            />
-                            :
-                            <img
-                                className='topImage'
-                                src={'/images/profile.png'}
-                                alt="avatar"
-                            />
-                        }
-                    </Link>
-                ) : ( 
-                    <ul className='topList'>
-                        <li className='topListItem'>
-                            <Link to={'/login'}>Login</Link>
-                        </li>
-                        <li className='topListItem'>
-                            <Link to={'/register'}>Register</Link>
-                        </li>
-                    </ul>
-                )}
-                <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
-            </div>
-        </div>
-    )
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <Link
+            to={"/settings"}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "3px",
+            }}
+          >
+            {user.profileImage ? (
+              <img
+                className="topImage"
+                src={publicFolder + user.profileImage}
+                alt="avatar"
+              />
+            ) : (
+              <img
+                className="topImage"
+                src={"/images/profile.png"}
+                alt="avatar"
+              />
+            )}
+            <div className="topbarUsername">{user?.name || "Username"}</div>
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link to={"/login"}>Login</Link>
+            </li>
+            <li className="topListItem">
+              <Link to={"/register"}>Register</Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
 }
