b2d23967bd77df716cf11eeefc6be3622cf86699
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-15 00:25:19 +0200

    Sidebar | Image | Header update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSmall">This is a blogging app</span>
+        {/* <span className="headerTitleSmall">This is a blogging app</span> */}
         <span className="headerTitleLarge">WELCOME</span>
       </div>
       <img className="headerImage" src={image} alt="" />
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,54 +1,53 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
-import http from '../../utils/axios';
-import './sidebar.css'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import http from "../../utils/axios";
+import "./sidebar.css";
+import image from "../../images/about.jpg";
 
 export default function Sidebar() {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        const fetchCategories = async () => {
-            const response = await http.get('/category');
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await http.get("/category");
 
-            setCategories(response.data);
-        }
-        fetchCategories();
-    }, []);
+      setCategories(response.data);
+    };
+    fetchCategories();
+  }, []);
 
-    return (
-        <aside className='sidebar'>
-            <div className="sidebarItem">
-                <span className="sidebarTitle">ABOUT ME</span>
-                <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg" alt="" />
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Repellat veniam, pariatur quos similique ex dolorum quae
-                    perferendis quas optio! Aut!
-                </p>
-            </div>
+  return (
+    <aside className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img src={image} alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat
+          veniam, pariatur quos similique ex dolorum quae perferendis quas
+          optio! Aut!
+        </p>
+      </div>
 
-            <div className='sidebarItem'>
-                <span className="sidebarTitle">CATEGORIES</span>
-                <ul className='sidebarList'>
-                    {categories.map((category, index) => (
-                        <li className="sidebarListItem" key={index}>
-                            <Link to={`/?category=${category?.name}`}>
-                                {category?.name}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {categories.map((category, index) => (
+            <li className="sidebarListItem" key={index}>
+              <Link to={`/?category=${category?.name}`}>{category?.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-            <div className='sidebarItem'>
-                <span className="sidebarTitle">FOLLOW US</span>
-                <div className="sidebarSocial">
-                    <i className="sidebarIcon fa-brands fa-facebook-square"></i>
-                    <i className="sidebarIcon fa-brands fa-twitter-square"></i>
-                    <i className="sidebarIcon fa-brands fa-pinterest-square"></i>
-                    <i className="sidebarIcon fa-brands fa-instagram-square"></i>
-                </div>
-            </div>
-        </aside>
-    )
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-facebook-square"></i>
+          <i className="sidebarIcon fa-brands fa-twitter-square"></i>
+          <i className="sidebarIcon fa-brands fa-pinterest-square"></i>
+          <i className="sidebarIcon fa-brands fa-instagram-square"></i>
+        </div>
+      </div>
+    </aside>
+  );
 }
