8fda39e25f5b0a0cbc07b4980bd4d841ab46c81c
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-15 19:56:37 +0200

    misc updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,6 @@ import image from "../../images/hero-main-pic.jpg";
 export default function Header() {
   return (
     <div className="header">
-      <div className="headerTitles">
-        {/* <span className="headerTitleSmall">This is a blogging app</span> */}
-        <span className="headerTitleLarge">WELCOME</span>
-      </div>
       <img className="headerImage" src={image} alt="" />
     </div>
   );
