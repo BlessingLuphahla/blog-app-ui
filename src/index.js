faff1934d7f6c36950449aa14dfa7e681c5d8cc7
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-15 19:33:11 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 // REACT VERSION 18.
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { ContextProvider } from './context/Context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { ContextProvider } from "./context/Context";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
-		<ContextProvider>
-			<App />
-		</ContextProvider>
-	</BrowserRouter>
-)
+  <BrowserRouter>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </BrowserRouter>
+);
