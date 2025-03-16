14e1a24ce0136526032c5e3024b0b9ac2090eb18
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-16 15:36:04 +0200

    create update

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -57,73 +57,75 @@ export default function Create() {
 
   return (
     <div className="create">
-      {filePreview && isOpenFilePreview && (
-        <div className="previewContainer">
-          <div
-            className="previewContainerClose"
-            onClick={() => setIsOpenFilePreview(false)}
-          >
-            ×
-          </div>
-          <img
-            className="createImage"
-            src={URL.createObjectURL(filePreview)}
-            alt="ss"
-          />
-        </div>
-      )}
-      <form className="createForm" onSubmit={handleSubmit(onSubmit)}>
-        <div className="createFormGroup">
-          <div className="createFormGroupOptions">
-            <input
-              type="file"
-              id="fileInput"
-              {...register("file")}
-              onChange={onSelectFile}
+      <div className="createSection">
+        {filePreview && isOpenFilePreview && (
+          <div className="previewContainer">
+            <div
+              className="previewContainerClose"
+              onClick={() => setIsOpenFilePreview(false)}
+            >
+              ×
+            </div>
+            <img
+              className="createImage"
+              src={URL.createObjectURL(filePreview)}
+              alt="ss"
             />
+          </div>
+        )}
+        <form className="createForm" onSubmit={handleSubmit(onSubmit)}>
+          <div className="createFormGroup">
+            <div className="createFormGroupOptions">
+              <input
+                type="file"
+                id="fileInput"
+                {...register("file")}
+                onChange={onSelectFile}
+              />
+              <input
+                type="text"
+                placeholder="Enter categories (should be comma seperated)"
+                className="createInput category"
+                autoFocus={true}
+                {...register("category", { required: true, minLength: 5 })}
+                onChange={(e) => setCategories(e.target.value)}
+              />
+            </div>
+
             <input
               type="text"
-              placeholder="Enter categories (should be comma seperated)"
-              className="createInput category"
+              placeholder="Title"
+              className="createInput"
               autoFocus={true}
-              {...register("category", { required: true, minLength: 5 })}
-              onChange={(e) => setCategories(e.target.value)}
+              {...register("title", { required: true, minLength: 5 })}
             />
           </div>
-
-          <input
-            type="text"
-            placeholder="Title"
-            className="createInput"
-            autoFocus={true}
-            {...register("title", { required: true, minLength: 5 })}
-          />
-        </div>
-        <div className="createFormGroup error">
-          {errors.title?.type === "required" && (
-            <span>This field is required</span>
-          )}
-          {errors.title?.type === "minLength" && (
-            <span>This field should be more than 4 symbols</span>
-          )}
-        </div>
-        <div className="createFormGroup">
-          <textarea
-            className="createInput createText"
-            placeholder="Tell your story..."
-            rows={10}
-            {...register("description", { required: true })}
-          ></textarea>
-        </div>
-        <div className="createFormGroup error">
-          {errors.description?.type === "required" && (
-            <span>This field is required</span>
-          )}
-        </div>
-        <button className="createSubmit" type="submit">
-          Publish
-        </button>
-      </form>
+          <div className="createFormGroup error">
+            {errors.title?.type === "required" && (
+              <span>This field is required</span>
+            )}
+            {errors.title?.type === "minLength" && (
+              <span>This field should be more than 4 symbols</span>
+            )}
+          </div>
+          <div className="createFormGroup">
+            <textarea
+              className="createInput createText"
+              placeholder="Tell your story..."
+              rows={10}
+              {...register("description", { required: true })}
+            ></textarea>
+          </div>
+          <div className="createFormGroup error">
+            {errors.description?.type === "required" && (
+              <span>This field is required</span>
+            )}
+          </div>
+          <button className="createSubmit" type="submit">
+            Publish
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
