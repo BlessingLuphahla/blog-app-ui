c36ea73dd532b94c19585196f5566b05f3d05dbd
Tapiwa128 <106350924+Tapiwa128@users.noreply.github.com>
2025-03-17 02:43:48 +0200

    Update About.jsx
    about section translation

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -5,14 +5,14 @@ function About() {
   return (
     <div className="about">
       <div className="aboutItem">
-        <span className="aboutTitle">ABOUT ME</span>
+        <span className="aboutTitle">ÜBER MICH</span>
         <p className="aboutParagraph">
         Willkommen auf Alex' Blog, deinem ultimativen Guide, um die Welt Reiseziel für Reiseziel zu entdecken! Ich bin ein leidenschaftliches Team aus Reisenden, Autoren und Fotografen, die an die transformative Kraft des Reisens glauben. Mein Ziel ist es, meine Erfahrungen, Erkenntnisse und Tipps zu teilen und dich zu deinen eigenen Abenteuern zu inspirieren. Ob du einen Kurztrip am Wochenende planst oder die Reise deines Lebens planst – Alex' Blog bietet detaillierte Reiseführer, Expertenempfehlungen und kuratierte Reiserouten, die dich auf jedem Schritt deiner Reise begleiten. Von pulsierenden Städtereisen bis hin zu ruhigen Rückzugsorten in der Natur – ich decke die unterschiedlichsten Reiseziele für jeden Reisetyp ab. Werde Teil meiner Community und entdecke die Wunder der Welt durch unsere Geschichten, Fotos und Reiseressourcen. Bereit, deine Koffer zu packen? Lass dich von uns zu deinem nächsten unvergesslichen Reiseziel führen. Meine Mission: Entdeckerfreude fördern, Neugier wecken und Reisende inspirieren – mit authentischen Reiseinhalten und praktischen Tipps. Meine Vision: Die führende Quelle der Inspiration und Orientierung für Reisende weltweit zu werden und eine Community leidenschaftlicher Entdecker zu fördern.
         </p>
         <br />
         <br />
         <div className="aboutItem">
-          <span className="aboutTitle">FOLLOW ME</span>
+          <span className="aboutTitle">FOLGEN SIE MIR</span>
           <p className="aboutParagraph">
             {" "}
             Bleiben Sie über die sozialen Medien mit mir in Verbindung und verpassen Sie keine Blogbeiträge, Reisetipps und Sonderangebote mehr, um Ihre Reiseerlebnisse zu verbessern. Ich glaube, beim Reisen geht es nicht nur darum, neue Orte zu besuchen, sondern auch darum, persönlich zu wachsen, neue Kulturen kennenzulernen und unvergessliche Erinnerungen zu schaffen. Auf Alex' Blog lade ich Sie ein, mich auf dieser unglaublichen Reise zu begleiten. Lassen Sie uns gemeinsam wandern!
@@ -21,10 +21,9 @@ function About() {
         <br />
         <br />
         <div className="aboutItem">
-          <span className="aboutTitle">MY MISSION</span>
+          <span className="aboutTitle">MEINE MISSION</span>
           <p className="aboutParagraph">
-            To encourage exploration, ignite curiosity, and inspire travelers by
-            providing authentic travel content and practical advice.
+          Wir möchten zum Entdecken anregen, die Neugier wecken und Reisende inspirieren, indem wir authentische Reiseinhalte und praktische Tipps bereitstellen.
           </p>
         </div>
       </div>
