1eecfbd629cee45bc1af3d6d21dad324aec5de18
Tapiwa128 <106350924+Tapiwa128@users.noreply.github.com>
2025-03-21 15:10:10 +0200

    Login page restructure

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,64 +1,67 @@
-import { useContext, useRef } from 'react';
-import { Link } from 'react-router-dom'
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { Context } from '../../context/Context';
 import http from '../../utils/axios';
-import './login.css'
+import './login.css';
 
 export default function Login() {
-	const nameRef = useRef();
-	const passwordRef = useRef();
-	const {dispatch, isFetching} = useContext(Context);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const { dispatch, isFetching } = useContext(Context);
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        dispatch({ type: 'LOGIN_START' });
 
-		dispatch({type: 'LOGIN_START'});
+        try {
+            const response = await http.post('/auth/login', {
+                username,
+                password
+            });
 
-		try {
-			const name = nameRef.current.value;
-			const password = passwordRef.current.value;
-	
-			const response = await http.post('/auth/login', {
-				username: name,
-				password: password
-			});
+            if (response.data) {
+                dispatch({ type: 'LOGIN_SUCCESS', payload: response.data });
+            }
+        } catch (error) {
+            dispatch({ type: 'LOGIN_FAILURE' });
+        }
+    };
 
-			if (response.data) {
-				dispatch({type: 'LOGIN_SUCCESS', payload: response.data});
-			}
-
-		} catch(error) {
-			dispatch({type: 'LOGIN_FAILURE'});
-		}
-	}
-
-	return (
-		<div className='login'>
-			<h1 className='loginTitle'>Login</h1>
-			<form className="loginForm" onSubmit={handleSubmit}>
-				<label>Username</label>
-				<input
-					type="text"
-					placeholder='Enter your username...'
-					ref={nameRef}
-				/>
-				<label>Password</label>
-				<input
-					type="password"
-					placeholder='Enter your password...'
-					ref={passwordRef}
-				/>
-				<button
-					className="loginButton"
-					type='submit'
-					disabled={isFetching}
-				>
-					Login
-				</button>
-			</form>
-			<button className='loginRegisterButton'>
-				<Link to={'/register'}>Register</Link>
-			</button>
-		</div>
-	)
+    return (
+        <div className='login'>
+            <div className='login-container'>
+                <h1 className='login-title'>Login</h1>
+                <form className='login-form' onSubmit={handleSubmit}>
+                    <div>
+                        <label>Username</label>
+                        <input
+                            type='text'
+                            placeholder='Enter your username...'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input
+                            type='password'
+                            placeholder='Enter your password...'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        type='submit'
+                        disabled={isFetching}
+                        className='login-button'
+                    >
+                        Login
+                    </button>
+                </form>
+                <div className='login-register'>
+                    <Link to='/register'>Don't have an account? Register</Link>
+                </div>
+            </div>
+        </div>
+    );
 }
