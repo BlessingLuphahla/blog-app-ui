a580aeeaf55263600b7d1c5a31303d887c360c7a
Tapiwa128 <106350924+Tapiwa128@users.noreply.github.com>
2025-03-22 13:14:18 +0200

    Slide Show
    a new slide show

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,35 @@
 import "./header.css";
-import image from "../../images/hero-main-pic.jpg";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation, Autoplay } from "swiper/modules";
+
+import image1 from "../../images/hero-main-pic.jpg";
+import image2 from "../../images/IMG_1951-scaled.jpeg";
+import image3 from "../../images/IMG_5495-scaled.jpeg";
 
 export default function Header() {
   return (
     <div className="header">
-      <img className="headerImage" src={image} alt="Travel Blog Banner" />
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        navigation
+        autoplay={{ delay: 3000 }}
+        loop
+        className="headerSwiper"
+      >
+        <SwiperSlide>
+          <img className="headerImage" src={image1} alt="Slide 1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="headerImage" src={image2} alt="Slide 2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="headerImage" src={image3} alt="Slide 3" />
+        </SwiperSlide>
+      </Swiper>
+
       <div className="headerTitle">
         <h1>Welcome To</h1>
         <h2>It's Travels Blog</h2>
